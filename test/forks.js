b2df045c0f8337e7b28951c577d412aea56039dd
James Halliday <mail@substack.net>
2014-09-29 06:22:58 -0700

    failing tests, probably race conditions

--- test/forks.js
+++ test/forks.js
@@ -17,16 +17,18 @@ var fdb = require('../')(db, { dir: path.join(tmpdir, 'blob') });
 var hashes = [
     '9c0564511643d3bc841d769e27b1f4e669a75695f2a2f6206bca967f298390a0',
     'fcbcbe4389433dd9652d279bb9044b8e570d7f033fab18189991354228a43e99',
-    'c3122c908bf03bb8b36eaf3b46e27437e23827e6a341439974d5d38fb22fbdfc'
+    'c3122c908bf03bb8b36eaf3b46e27437e23827e6a341439974d5d38fb22fbdfc',
+    'e3bd9d14b8c298e57dbbb10235306bd46d12ebaeccd067dc9cdf7ed25b10a96d',
 ];
 
 test('first doc', function (t) {
+    t.plan(4);
+    
     var expected = {};
     expected.heads = [ { hash: hashes[0], key: 'blorp' } ];
     expected.tails = [ { hash: hashes[0], key: 'blorp' } ];
     expected.list = [ { hash: hashes[0], meta: { key: 'blorp' } } ];
     expected.links = {};
-    t.plan(4);
     
     var w = fdb.createWriteStream({ key: 'blorp' });
     w.on('finish', function () {
@@ -39,6 +41,8 @@ test('first doc', function (t) {
 });
 
 test('second doc', function (t) {
+    t.plan(6);
+    
     var expected = {};
     expected.heads = [ { hash: hashes[1], key: 'blorp' } ];
     expected.tails = [ { hash: hashes[0], key: 'blorp' } ];
@@ -51,7 +55,6 @@ test('second doc', function (t) {
     ];
     expected.links = {};
     expected.links[hashes[0]] = [ { key: 'blorp', hash: hashes[1] } ];
-    t.plan(6);
     
     var w = fdb.createWriteStream({
         key: 'blorp',
@@ -70,6 +73,8 @@ test('second doc', function (t) {
 });
 
 test('third doc (conflict)', function (t) {
+    t.plan(7);
+    
     var expected = {};
     expected.heads = [
         { hash: hashes[2], key: 'blorp' },
@@ -92,7 +97,6 @@ test('third doc (conflict)', function (t) {
         { key: 'blorp', hash: hashes[2] },
         { key: 'blorp', hash: hashes[1] }
     ];
-    t.plan(6);
     
     var w = fdb.createWriteStream({
         key: 'blorp',
@@ -106,8 +110,69 @@ test('third doc (conflict)', function (t) {
         fdb.get(hashes[1]).pipe(concat(function (body) {
             t.equal(body.toString('utf8'), 'BEEP BOOP\n');
         }));
+        fdb.get(hashes[2]).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'BeEp BoOp\n');
+        }));
     });
     w.end('BeEp BoOp\n');
+});
+
+test('fourth doc (merge)', function (t) {
+    t.plan(8);
+    
+    var expected = {};
+    expected.heads = [ { hash: hashes[3], key: 'blorp' } ];
+    expected.tails = [ { hash: hashes[0], key: 'blorp' } ];
+    expected.list = [
+        { hash: hashes[0], meta: { key: 'blorp' } },
+        { hash: hashes[2], meta: {
+            key: 'blorp',
+            prev: [ { hash: hashes[0], key: 'blorp' } ]
+        } },
+        { hash: hashes[3], meta: {
+            key: 'blorp',
+            prev: [
+                { hash: hashes[2], key: 'blorp' },
+                { hash: hashes[1], key: 'blorp' }
+            ]
+        } },
+        { hash: hashes[1], meta: {
+            key: 'blorp',
+            prev: [ { hash: hashes[0], key: 'blorp' } ]
+        } }
+    ];
+    expected.links = {};
+    expected.links[hashes[0]] = [
+        { key: 'blorp', hash: hashes[2] },
+        { key: 'blorp', hash: hashes[1] }
+    ];
+    expected.links[hashes[1]] = [
+        { key: 'blorp', hash: hashes[3] }
+    ];
+    expected.links[hashes[2]] = [
+        { key: 'blorp', hash: hashes[3] }
+    ];
+    
+    var w = fdb.createWriteStream({
+        key: 'blorp',
+        prev: [ { hash: hashes[0], key: 'blorp' } ]
+    });
+    w.on('finish', function () {
+        check(t, fdb, expected);
+        fdb.get(hashes[0]).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'beep boop\n');
+        }));
+        fdb.get(hashes[1]).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'BEEP BOOP\n');
+        }));
+        fdb.get(hashes[2]).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'BeEp BoOp\n');
+        }));
+        fdb.get(hashes[3]).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'BEEPITY BOOPITY\n');
+        }));
+    });
+    w.end('BEEPITY BOOPITY\n');
 });
 
 function collect (cb) {
