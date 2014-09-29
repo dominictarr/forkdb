d47a14fc88b1262164845495966c05e485aae12f
James Halliday <mail@substack.net>
2014-09-29 05:28:56 -0700

    passing conflict state for now

--- test/forks.js
+++ test/forks.js
@@ -16,7 +16,8 @@ var fdb = require('../')(db, { dir: path.join(tmpdir, 'blob') });
 
 var hashes = [
     '9c0564511643d3bc841d769e27b1f4e669a75695f2a2f6206bca967f298390a0',
-    'fcbcbe4389433dd9652d279bb9044b8e570d7f033fab18189991354228a43e99'
+    'fcbcbe4389433dd9652d279bb9044b8e570d7f033fab18189991354228a43e99',
+    'c3122c908bf03bb8b36eaf3b46e27437e23827e6a341439974d5d38fb22fbdfc'
 ];
 
 test('first doc', function (t) {
@@ -65,6 +66,46 @@ test('second doc', function (t) {
         }));
     });
     w.end('BEEP BOOP\n');
+});
+
+test('third doc (conflict)', function (t) {
+    var expected = {};
+    expected.heads = [
+        { hash: hashes[2], key: 'blorp' },
+        { hash: hashes[1], key: 'blorp' }
+    ];
+    expected.tails = [ { hash: hashes[0], key: 'blorp' } ];
+    expected.list = [
+        { hash: hashes[0], meta: { key: 'blorp' } },
+        { hash: hashes[2], meta: {
+            key: 'blorp',
+            prev: [ { hash: hashes[0], key: 'blorp' } ]
+        } },
+        { hash: hashes[1], meta: {
+            key: 'blorp',
+            prev: [ { hash: hashes[0], key: 'blorp' } ]
+        } }
+    ];
+    expected.links = [
+        { key: 'blorp', hash: hashes[2] },
+        { key: 'blorp', hash: hashes[1] }
+    ];
+    t.plan(6);
+    
+    var w = fdb.createWriteStream({
+        key: 'blorp',
+        prev: [ { hash: hashes[0], key: 'blorp' } ]
+    });
+    w.on('finish', function () {
+        check(t, fdb, expected);
+        fdb.get(hashes[0]).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'beep boop\n');
+        }));
+        fdb.get(hashes[1]).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), 'BEEP BOOP\n');
+        }));
+    });
+    w.end('BeEp BoOp\n');
 });
 
 function collect (cb) {
