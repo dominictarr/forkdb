c33dbc23f71cf453cb424e10ad6f58b8ea81fc2b
James Halliday <mail@substack.net>
2014-10-05 18:42:49 -0700

    blob test for more coverage

---
+++ test/blob.js
@@ -0,0 +1,28 @@
+var test = require('tape');
+var path = require('path');
+var level = require('level');
+var mkdirp = require('mkdirp');
+var concat = require('concat-stream');
+
+var tmpdir = path.join(
+    require('osenv').tmpdir(),
+    'forkdb-test-' + Math.random()
+);
+mkdirp.sync(tmpdir);
+
+var db = level(path.join(tmpdir, 'db'));
+var forkdb = require('../');
+var fdb = forkdb(db, { dir: path.join(tmpdir, 'blob') });
+
+var blob = Array(1000 * 20 + 1).join('A');
+
+test('blob', function (t) {
+    t.plan(2);
+    var w = fdb.createWriteStream({}, function (err, key) {
+        t.ifError(err);
+        fdb.get(key).pipe(concat(function (body) {
+            t.equal(body.toString('utf8'), blob);
+        }));
+    });
+    w.end(blob);
+});
