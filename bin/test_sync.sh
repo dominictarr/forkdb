f5784963c243e37a2e1bc5a328886301998c68a8
James Halliday <mail@substack.net>
2014-10-20 08:19:36 -0700

    everything was fine with replication after all

--- bin/test_sync.sh
+++ bin/test_sync.sh
@@ -6,4 +6,4 @@ echo ZING | forkdb -d /tmp/a create yo --prev=55a0f917f274311f54874dc7af9682e7b6
 echo WOOOOOOO | forkdb -d /tmp/b create yo --prev=55a0f917f274311f54874dc7af9682e7b625846286ecce4056ab1a2d88489df7
 dupsh 'forkdb -d /tmp/a sync' 'forkdb -d /tmp/b sync'
 echo '**********************';
-forkdb -d /tmp/a heads
+forkdb -d /tmp/a heads yo
